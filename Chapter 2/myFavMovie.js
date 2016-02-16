1147ac606708fafe02a34cccc5d174d0005d8a01
Sarah <sarahjanaskie@gmail.com>
2016-02-15 20:47:37 -0500

    Initial Commit
    Finished chapters 1-8

---
+++ Chapter 2/myFavMovie.js
@@ -0,0 +1,11 @@
+//Sarah Janaskie
+document.write("Dom Cobb, Leonardo DiCaprio, is a skilled thief, the absolute best in the dangerous art of extraction, "
+              + "stealing valuable secrets from deep within the subconscious, during the dream state when the mind is most vulnerable "
+              + "Cobb's rare ability has made him a coveted player in this treacherous new world of corporate espionage, "
+              + "but it has also made him an international fugitive and cost him everything he has ever loved. "
+              + "Now Cobb is being offered a chance at redepmtion. "
+              + "One last job could give him his life back but only if he can accomplish the impossible: inception. "
+              + "Instead of the perfect heist, Cobb and his team of specialists have to pull off the reverse: their task "
+              + "is not to steal an idea but to plant one. If they succded, it could be the perfect crime. But no amount "
+              + "of careful planning or expterise can prepare the team for the dangerous enemy that seems to predict their "
+              + "every move. An enemy that only Cobb could have seen coming.") //Content used from Warner Bros description of the movie
